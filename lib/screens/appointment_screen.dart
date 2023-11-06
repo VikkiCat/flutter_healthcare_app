605855e5b2536fe7cf9453c530d4408828b6595b
Viktoria Henyk <viktoia.genuk@mail.com>
2023-11-06 18:29:54 +0100

    Appointment part1

--- lib/screens/appointment_screen.dart
+++ lib/screens/appointment_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-//ToDo: 43:34
+//ToDo: 49:12
 
 class AppointmentScreen extends StatelessWidget {
-  const AppointmentScreen({super.key});
-
   List imgs = [
     "doctor1.jpg",
     "doctor2.jpg",
@@ -13,6 +11,92 @@ class AppointmentScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Color(0xFF7165D6),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 5),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Stack(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage: AssetImage("imaes/doctor1.jpg"),
+                        ),
+                        SizedBox(height: 15),
+                        Text(
+                          "Dr. Doctor Name",
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Text(
+                          "Therapist",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: Color(0xFF9F97E2),
+                                shape: BoxShape.circle,
+                              ),
+                              child: Icon(
+                                Icons.call,
+                                color: Colors.white,
+                                size: 25,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
